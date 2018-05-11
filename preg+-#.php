274784ea2fb7f7bb9a06754266887bea98652d63
you <873922593@qq.com>
2018-05-11 17:28:00 +0800

    preg_str

---
+++ preg+-#.php
@@ -0,0 +1,25 @@
+<?php 
+   
+
+    /**
+     *从独立的小括号开始匹配
+     *
+     */
+	$str = 5+6+(7*8)+((10-1)/3);
+
+	$str = preg_replace_callback('/\(([^\(])\)/', function($matches) {
+	
+		return prege_str($matches[1]);
+	
+	},$str);
+
+	function prege_str($str) {
+		while(strpos($str,'*') !== false || strpos($str,'/') !== false){
+			$str = preg_replace_callback('/([^\+\-\*\/])([\*\/])([^\+\-\*\/])/',function($matches){
+				return ($matches[2]== '*' ? 'bcsub' : 'bcdiv')."[{$matches[1]},{$matches[3]}]";
+			}
+			
+			);
+		}
+
+	}
